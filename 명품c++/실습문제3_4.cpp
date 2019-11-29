f8a428e36915d2d76953f9dc432925f4b3d19651
su-mmer <50980974+su-mmer@users.noreply.github.com>
2019-11-29 21:43:29 +0900

    Create 실습문제3_4.cpp

---
+++ 명품c++/실습문제3_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    string address("서울시 성북구");
+    string copyAddress(address);
+
+    char text[] = {'L', 'o', 'v', 'e', ' ', 'C', '+', '+', '\0'};
+    string title(text);
+
+    cout << str << endl;
+    cout << address << endl;
+    cout << copyAddress << endl;
+    cout << title << endl;
+}
