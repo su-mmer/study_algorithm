50e0ecf3ba6a544a9a2415b1845e34dce1f3de85
su-mmer <50980974+su-mmer@users.noreply.github.com>
2019-11-29 21:44:30 +0900

    Update 실습문제3_4.cpp

--- 명품c++/실습문제3_4.cpp
+++ 명품c++/실습문제3_4.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class CoffeeMachine {
+	int c, w, s;
+public:
+	CoffeeMachine(int c, int w, int s);
+	void drinkEspresso() { c -= 1; w -= 1; }
+	void drinkAmericano() { c -= 1; w -= 2; }
+	void drinkSugarCoffee() { c -= 1; w -= 2; s -= 1; }
+	void fill() { c = 10; w = 10; s = 10; }
+	void show() {
+		cout << "커피 머신 상태, 커피:" << c << "\t물:" << w << "\t설탕:" << s << endl;
+	}
+};
+
+CoffeeMachine::CoffeeMachine(int c, int w, int s) : c(c), w(w), s(s){}
+
 int main() {
-    string str;
-    string address("서울시 성북구");
-    string copyAddress(address);
-
-    char text[] = {'L', 'o', 'v', 'e', ' ', 'C', '+', '+', '\0'};
-    string title(text);
-
-    cout << str << endl;
-    cout << address << endl;
-    cout << copyAddress << endl;
-    cout << title << endl;
+	CoffeeMachine java(5, 10, 3);
+	java.drinkEspresso();
+	java.show();
+	java.drinkAmericano();
+	java.show();
+	java.drinkSugarCoffee();
+	java.show();
+	java.fill();
+	java.show();
 }
