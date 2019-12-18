9ace9a0c8200729cc7b3a7a98e59e2839df534dc
su-mmer <50980974+su-mmer@users.noreply.github.com>
2019-12-18 17:27:30 +0900

    Create 실습문제4_06.cpp

---
+++ 명품c++/실습문제4_06.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "아래에 한 줄을 입력하세요.(exit를 입력하면 종료합니다)" << endl;
+	string str;
+
+	while (true) {
+		cout << ">>";
+		getline(cin, str, '\n');
+		if (str == "exit") break;
+
+		for (int i = str.length(); i >= 0; i--) {
+			cout << str[i];
+		}
+		cout << endl;
+	}
+}
